25030236b4851d371a40e02f4b7ae00f6069f19b
Kanefav <gabrielfav12@gmail.com>
2021-10-22 14:59:16 -0300

    Sudoku Solution Validator

--- Challenge13.py
+++ Challenge13.py
@@ -1,61 +1,26 @@
 def valid_solution(board):
-    save = []
-    firsty = cont = sum = firstx = 0
-    secondy = secondx = 3
+    sum = 0
     for linha in board:
         lin = str(linha)
         if lin.count('0') >= 1:
             return False
-    for linha in board:
-        if cont == 1:
-            firsty = 3
-            secondy = 6
-        if cont == 2:
-            firsty = 6
-            secondy = 9
-        if cont == 3:
-            firsty = 0
-            secondy = 3
-            firstx = 3
-            secondx = 6
-        if cont == 4:
-            firsty = 3
-            secondy = 6
-        if cont == 5:
-            firsty = 6
-            secondy = 9
-        if cont == 6:
-            firsty = 0
-            secondy = 3
-            firstx = 6
-            secondx = 9
-        if cont == 7:
-            firsty = 3
-            secondy = 6
-        if cont == 8:
-            firsty = 6
-            secondy = 9
-        for x in range(firstx, secondx):
-            for y in range(firsty, secondy):
-                save.append(board[x][y])
+    for linha in range(0, 9, 3):
+        for x in range(linha, linha+3):
+            for y in range(linha, linha+3):
                 sum += board[x][y]
         if sum != 45:
             return False
         else:
-            print(save)
-            print(sum)
-            cont += 1
-            save = []
             sum = 0
     return True
 
 
-print(valid_solution([ [1, 3, 2, 5, 7, 9, 4, 6, 8],
-[4, 9, 8, 2, 6, 1, 3, 7, 5],
-[7, 5, 6, 3, 8, 4, 2, 1, 9],
-[6, 4, 3, 1, 5, 8, 7, 9, 2],
-[5, 2, 1, 7, 9, 3, 8, 4, 6],
-[9, 8, 7, 4, 2, 6, 5, 3, 1],
-[2, 1, 4, 9, 3, 5, 6, 8, 7],
-[3, 6, 5, 8, 1, 7, 9, 2, 4],
-[8, 7, 9, 6, 4, 2, 1, 3, 5] ]))
+print(valid_solution(             [[5, 3, 4, 6, 7, 8, 9, 1, 2], 
+                                   [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                                   [1, 9, 8, 3, 4, 2, 5, 6, 7],
+                                   [8, 5, 9, 7, 6, 1, 4, 2, 3],
+                                   [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                                   [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                                   [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                                   [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                                   [3, 4, 5, 2, 8, 6, 1, 7, 9]]))
