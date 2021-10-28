6203c6f028937a01c1ef568ba77d1590d8517dba
Kanefav <gabrielfav12@gmail.com>
2021-10-28 15:18:36 -0300

    Count a Spiral (nw cuz of ms with large numbers)

---
+++ Challenge21.py
@@ -0,0 +1,22 @@
+def spiral_sum(size):
+    rsize = size
+    outsize = size*size
+    soma = 0
+    for notwhite in range(1, size-1):
+        if notwhite == 1:
+            size -= notwhite
+            soma += size
+        elif notwhite % 2 == 0:
+            size -= 2
+            soma += size
+        else:
+            soma += size
+    if rsize < 10:
+        return outsize - soma
+    else:
+        return outsize - soma-1
+
+    
+
+
+print(spiral_sum(123456))
