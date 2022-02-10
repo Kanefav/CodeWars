530bfd0f53f06b59da74c28a4702f5db4953b89a
Kanefav <gabrielfav12@gmail.com>
2022-02-10 15:14:05 -0300

    Roman Numerals Decoder

--- Challenge08.py
+++ Challenge08.py
@@ -1,66 +1,23 @@
 def solution(roman):
-    num = 0
-    verificador = 0
-    verificador = 0
-    for letra in range(0, len(roman)):
-        numteste = num
-        proximo = letra + 1
-        if roman[letra] == 'I':
-            try:
-                if roman[proximo] == 'X':
-                    verificador = 1
-                    num += 9
-                elif roman[proximo] == 'V':
-                    verificador = 1
-                    num += 4
-                else:
-                    verificador = 0
-                    num += 1
-            except:
-                num += 1
-        if verificador == 0:
-            if roman[letra] == 'V':
-                num += 5
-        if verificador == 0:
-            if roman[letra] == 'X':
-                try:
-                    if roman[proximo] == 'L':
-                        verificador = 1
-                        num += 40
-                    elif roman[proximo] == 'C':
-                        verificador = 1
-                        num += 90
+        numbers = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+        romans = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+        last = 0
+        output = 0
+        for letter in roman:
+            for c in range(0, 13):
+                if letter == romans[c]:
+                    if last == 0: 
+                        last = numbers[c]   
+                        output += numbers[c]
                     else:
-                        verificador = 0
-                        num += 10
-                except:
-                    num += 10
-        if verificador == 0:
-            if roman[letra] == 'L':
-                num += 50
-        if verificador == 0:
-            if roman[letra] == 'C':
-                try:
-                    if roman[proximo] == 'M':
-                        verificador = 1
-                        num += 900
-                    elif roman[proximo] == 'D':
-                        verificador = 1
-                        num += 400
-                    else:
-                        verificador = 0 
-                        num += 100
-                except:
-                    num += 100
-        if verificador == 0:
-            if roman[letra] == 'D':
-                num += 500
-        if verificador == 0:
-            if roman[letra] == 'M':
-                num += 1000
-        if num == numteste:
-            verificador = 0
-    return num
+                        
+                        if last < numbers[c]: 
+                            output -= last
+                            output += numbers[c] - last
+                        else:
+                            output += numbers[c]
+                    last = numbers[c]
+        return output
 
 
 print(solution('MCMLXXXI')) #MCML
